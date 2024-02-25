07092e67bc8f76c1f439f2d14609d85050a522b6
Intaemik <kom5431@naver.com>
2024-02-25 14:21:27 +0900

    login_1

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,10 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(qwer) {
+  qwer.preventDefault();
+  // const username = loginInput.value;
+  console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
